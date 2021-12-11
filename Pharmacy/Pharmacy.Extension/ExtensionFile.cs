be68772d6a62771a199d1503c765dac2e8840e3e
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:16:33 +0300

    Added Extension

---
+++ Pharmacy/Pharmacy.Extension/ExtensionFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Pharmacy.Extension
+{
+    public static class ExtensionFile
+    {
+        //Get enum attribute
+        public static string GetEnum(this Enum _variable)
+        {
+            var result = _variable.GetType().GetMember(_variable.ToString()).First().GetCustomAttributes<DisplayAttribute>().First().Name; 
+            return result;
+        }
+
+        //Para birimi eklemek için extension oluşturuldu
+        public static string AddToCurrency(this int item)
+        {
+            return  item  + " TRY";
+        }
+    }
+}
