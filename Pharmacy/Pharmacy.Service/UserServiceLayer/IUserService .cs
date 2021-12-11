f5fe83bfc0640c65eb3988361827829a08625f38
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:12:25 +0300

    Created user service and user interface

--- Pharmacy/Pharmacy.Service/UserServiceLayer/IUserService .cs
+++ Pharmacy/Pharmacy.Service/UserServiceLayer/IUserService .cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pharmacy.Model;
+using Pharmacy.Model.ModelUser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Pharmacy.Service.UserServiceLayer
 {
     public interface IUserService
     {
+        public bool Login(string mail, string password);
+        public General<UserViewModel> Insert(UserViewModel newUser);
+        public General<UserViewModel> GetUsers();
+        public General<UserViewModel> Update(int id, UserViewModel user);
+        public General<UserViewModel> Delete(int id);
+        public List<Pharmacy.DB.Entities.User> GetPatients();
     }
 }
