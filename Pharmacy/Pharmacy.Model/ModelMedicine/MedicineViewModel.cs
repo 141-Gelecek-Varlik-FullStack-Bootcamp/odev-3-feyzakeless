9300d63366279c7513398879dd418a2afcbe07d0
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:15:23 +0300

    Added medicine model file

---
+++ Pharmacy/Pharmacy.Model/ModelMedicine/MedicineViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Model.ModelMedicine
+{
+    public class MedicineViewModel
+    {
+        public string Name { get; set; }
+        public string TicketCode { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int Iuser { get; set; }
+    }
+}
