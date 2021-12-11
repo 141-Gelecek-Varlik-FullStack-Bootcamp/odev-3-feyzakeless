ce44726af171aacc9224e72274007afe4024ed1e
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:21:37 +0300

    Added changes to prescription controller

--- Pharmacy/Pharmacy.API/Controllers/PrescriptionController.cs
+++ Pharmacy/Pharmacy.API/Controllers/PrescriptionController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Model;
+using Pharmacy.Model.ModelPrescription;
+using Pharmacy.Service.PrescriptionServiceLayer;
 
 namespace Pharmacy.API.Controllers
 {
@@ -7,5 +11,44 @@ namespace Pharmacy.API.Controllers
     [ApiController]
     public class PrescriptionController : ControllerBase
     {
+        private readonly IPrescriptionService PrescriptionService;
+        private readonly IMapper mapper;
+
+        public PrescriptionController(IPrescriptionService _PrescriptionService, IMapper _mapper)
+        {
+            PrescriptionService = _PrescriptionService;
+            mapper = _mapper;
+        }
+
+        //Recete Ekleme
+        [HttpPost]
+        public General<PrescriptionViewModel> Insert([FromBody] PrescriptionViewModel newPrescription)
+        {
+            var result = false;
+            return PrescriptionService.Insert(newPrescription);
+        }
+
+        //Recete Listeleme
+        [HttpGet]
+        public General<PrescriptionViewModel> GetPrescription()
+        {
+            return PrescriptionService.GetPrescription();
+        }
+
+        //Id ye gore recete guncelleme
+        [HttpPut("{id}")]
+        public General<PrescriptionViewModel> Update(int id, PrescriptionViewModel Prescription)
+        {
+            return PrescriptionService.Update(id, Prescription);
+        }
+
+
+        //Recete silme
+        [HttpDelete]
+        public General<PrescriptionViewModel> Delete(int id)
+        {
+            return PrescriptionService.Delete(id);
+        }
+
     }
 }
