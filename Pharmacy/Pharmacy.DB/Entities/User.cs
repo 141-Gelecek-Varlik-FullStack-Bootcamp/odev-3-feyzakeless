efa5e4803d0864f59f0a5b601fbc6147a4477442
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:18:43 +0300

    Added changes to user table

--- Pharmacy/Pharmacy.DB/Entities/User.cs
+++ Pharmacy/Pharmacy.DB/Entities/User.cs
@@ -10,20 +10,23 @@ namespace Pharmacy.DB.Entities
         public User()
         {
             Medicine = new HashSet<Medicine>();
+            Prescription = new HashSet<Prescription>();
         }
 
         public int Id { get; set; }
+        public int AuthorizeId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public bool IsActive { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime Idatetime { get; set; }
         public DateTime? Udatetime { get; set; }
         public int Iuser { get; set; }
         public int? Uuser { get; set; }
 
+        public virtual Enum Authorize { get; set; }
         public virtual ICollection<Medicine> Medicine { get; set; }
+        public virtual ICollection<Prescription> Prescription { get; set; }
     }
 }
