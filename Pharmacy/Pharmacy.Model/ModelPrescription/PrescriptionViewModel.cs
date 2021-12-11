9ca0f97d357265e5efebf6610e33b7e07084d927
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:15:12 +0300

    Added prescription model file

---
+++ Pharmacy/Pharmacy.Model/ModelPrescription/PrescriptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Model.ModelPrescription
+{
+    public class PrescriptionViewModel
+    {
+        public string Name { get; set; }
+        public int PrescriptionNo { get; set; }
+        public bool IsActive { get; set; }
+        public int Iuser { get; set; }
+        public int Imedicine { get; set; }
+    }
+}
