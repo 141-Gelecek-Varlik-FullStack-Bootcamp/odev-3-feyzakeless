9e045606be7aff36ee728d05aef2fc5ebf4ca65a
feyzakeless <feyzakeles@gmail.com>
2021-12-11 03:21:56 +0300

    Created DB layer

---
+++ Pharmacy/Pharmacy.DB/Entities/Medicine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Pharmacy.DB.Entities
+{
+    public partial class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TicketCode { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime Idate { get; set; }
+        public DateTime? Udate { get; set; }
+        public int Iuser { get; set; }
+        public int? Uuser { get; set; }
+
+        public virtual User IuserNavigation { get; set; }
+    }
+}
---
+++ Pharmacy/Pharmacy.DB/Entities/Prescription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Pharmacy.DB.Entities
+{
+    public partial class Prescription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PrescriptionNo { get; set; }
+        public int AuthorizeId { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public int Iuser { get; set; }
+        public int? Uuser { get; set; }
+        public DateTime Idate { get; set; }
+        public DateTime? Udate { get; set; }
+        public int Imedicine { get; set; }
+    }
+}
