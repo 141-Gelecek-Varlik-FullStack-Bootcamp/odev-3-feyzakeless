34d0a2d2590c049fc4674a7f6bc95ca5602f5a12
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:12:06 +0300

    Created medicine service and medicine interface

--- Pharmacy/Pharmacy.Service/MedicineServiceLayer/IMedicineService.cs
+++ Pharmacy/Pharmacy.Service/MedicineServiceLayer/IMedicineService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pharmacy.Model;
+using Pharmacy.Model.ModelMedicine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Pharmacy.Service.MedicineServiceLayer
 {
     public interface IMedicineService
     {
+        public General<MedicineViewModel> Insert(MedicineViewModel newmedicine);
+        public General<MedicineViewModel> GetMedicines();
+        public General<MedicineViewModel> Update(int id, MedicineViewModel medicine);
+        public General<MedicineViewModel> Delete(int id);
+
     }
 }
