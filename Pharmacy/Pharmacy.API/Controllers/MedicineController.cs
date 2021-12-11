448ff827da59e26880dc443d79f3b78538b060d3
feyzakeless <feyzakeles@gmail.com>
2021-12-12 01:21:50 +0300

    Added changes to medicine controller

--- Pharmacy/Pharmacy.API/Controllers/MedicineController.cs
+++ Pharmacy/Pharmacy.API/Controllers/MedicineController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Model;
+using Pharmacy.Model.ModelMedicine;
+using Pharmacy.Service.MedicineServiceLayer;
 
 namespace Pharmacy.API.Controllers
 {
@@ -7,5 +11,43 @@ namespace Pharmacy.API.Controllers
     [ApiController]
     public class MedicineController : ControllerBase
     {
+        private readonly IMedicineService medicineService;
+        private readonly IMapper mapper;
+
+        public MedicineController(IMedicineService _medicineService, IMapper _mapper)
+        {
+            medicineService = _medicineService;
+            mapper = _mapper;
+        }
+
+        //İlac Ekleme
+        [HttpPost]
+        public General<MedicineViewModel> Insert([FromBody] MedicineViewModel newProduct)
+        {
+            var result = false;
+            return medicineService.Insert(newProduct);
+        }
+
+        //İlac Listeleme
+        [HttpGet]
+        public General<MedicineViewModel> GetMedicines()
+        {
+            return medicineService.GetMedicines();
+        }
+
+        //İlac id sine gore guncelleme
+        [HttpPut("{id}")]
+        public General<MedicineViewModel> Update(int id, MedicineViewModel product)
+        {
+            return medicineService.Update(id, product);
+        }
+
+        //İlac silme
+        [HttpDelete]
+        public General<MedicineViewModel> Delete(int id)
+        {
+            return medicineService.Delete(id);
+        }
+
     }
 }
